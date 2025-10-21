4446e025f5cee98b40f0afd60f17adc4ed306bbe
cvega <carlosjuniorrvega@hotmail.com>
2025-10-21 15:28:19 -0500

    feat: add reusable BasePage class for common selenium actions. CV

---
+++ src/main/java/pages/base/BasePage.java
@@ -0,0 +1,31 @@
+package pages.base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public abstract class BasePage {
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    protected WebElement find(By locator) {
+        WebElement element = driver.findElement(locator);
+        return element;
+    }
+
+
+    protected void set(By locator, String value) {
+        WebElement element = find(locator);
+        element.sendKeys(value);
+    }
+
+
+    protected void click(By locator) {
+        find(locator).click();
+    }
+
+
+}

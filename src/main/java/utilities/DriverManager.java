40345ce98e8b45f1ac958060c221cab8abb8f3a9
cvega <carlosjuniorrvega@hotmail.com>
2025-10-21 14:05:58 -0500

    feat: Add centralized WebDriver manager class. CV

---
+++ src/main/java/utilities/DriverManager.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+    private static WebDriver driver;
+
+    public void initDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}

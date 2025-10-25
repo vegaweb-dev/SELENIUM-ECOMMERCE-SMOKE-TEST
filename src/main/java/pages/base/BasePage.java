19fdf62b9ca5702378847191b1b605aae3537a75
cvega <carlosjuniorrvega@hotmail.com>
2025-10-25 12:15:02 -0500

    refactor(BasePage): changed driver visibility to protected for subclass access. CV

--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -3,17 +3,27 @@ package pages.base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.DriverManager;
+
+import java.time.Duration;
 
 public abstract class BasePage {
-    private WebDriver driver;
+    protected WebDriver driver;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
 
+    protected WebDriver getDriver() {
+        return DriverManager.getDriver();
+    }
+
     protected WebElement find(By locator) {
-        WebElement element = driver.findElement(locator);
-        return element;
+        /*WebElement element = driver.findElement(locator);
+        return element;*/
+        return driver.findElement(locator);
     }
 
 

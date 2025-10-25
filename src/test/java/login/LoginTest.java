799d17d761fbe735f3de2f4d4c2768f7d22778e3
cvega <carlosjuniorrvega@hotmail.com>
2025-10-25 12:27:11 -0500

    feat(LoginTest): implement successfull login test. CV

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -1,4 +1,23 @@
 package login;
 
-public class LoginTest {
+import base.BaseTest;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.login.LoginPage;
+
+public class LoginTest extends BaseTest {
+    private LoginPage loginPage;
+
+    @Test
+    public void shouldLoginSuccessfullyAndRedirectToInventoryPage() {
+        String url = "https://www.saucedemo.com";
+        driver.get(url);
+        loginPage = new LoginPage(driver);
+        loginPage.setUser("standard_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLoginButton();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Login test failed: Expected user to be redirected to the Inventory page after successful login.");
+    }
 }

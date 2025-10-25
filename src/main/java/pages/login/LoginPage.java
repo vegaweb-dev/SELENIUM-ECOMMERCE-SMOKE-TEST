d07304a47489110097477abef8dbd6fb84afe27f
cvega <carlosjuniorrvega@hotmail.com>
2025-10-25 11:52:20 -0500

    refactor(LoginPage): remove private WebDriver driver for consistent use of correct driver coming from LoginTest. CV

--- src/main/java/pages/login/LoginPage.java
+++ src/main/java/pages/login/LoginPage.java
@@ -6,7 +6,6 @@ import pages.base.BasePage;
 import pages.inventory.InventoryPage;
 
 public class LoginPage extends BasePage {
-    private WebDriver driver;
     private By usernameField = By.id("user-name");
     private By passwordField = By.id("password");
     private By clickButton = By.id("login-button");
@@ -16,31 +15,36 @@ public class LoginPage extends BasePage {
         super(driver);
     }
 
+
     public void setUser(String user) {
+        waitForVisibility(usernameField, 10);
         set(usernameField, user);
     }
 
     public void setPassword(String password) {
+        waitForVisibility(passwordField, 10);
         set(passwordField, password);
     }
 
     public void enterCredentials(String user, String password) {
-        set(usernameField, user);
-        set(passwordField, password);
+        setUser(user);
+        setPassword(password);
     }
 
     public void clickLoginButton() {
+        waitForClickable(clickButton, 10);
         click(clickButton);
     }
 
     public String errorMessage(String user, String password) {
+        waitForVisibility(errorMessage, 10);
         setUser(user);
         setPassword(password);
         clickLoginButton();
         return "Epic sadface: Username and password do not match any user in this service";
     }
 
-    public InventoryPage loginPage(String user, String password){
+    public InventoryPage loginPage(String user, String password) {
         setUser(user);
         setPassword(password);
         clickLoginButton();

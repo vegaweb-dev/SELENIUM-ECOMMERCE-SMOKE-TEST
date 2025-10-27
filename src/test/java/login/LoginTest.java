5e3053c982526b8c1665bb3a4a3a4f9e567cddce
cvega <carlosjuniorrvega@hotmail.com>
2025-10-27 17:37:02 -0500

    chore(BaseTest): remove unnecessary lines. CV

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -1,22 +1,21 @@
 package login;
 
 import base.BaseTest;
-import net.bytebuddy.implementation.bytecode.Throw;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.login.LoginPage;
+import utilities.ConfigReader;
 
 public class LoginTest extends BaseTest {
     private LoginPage loginPage;
 
     @Test
     public void shouldLoginSuccessfullyAndRedirectToInventoryPage() {
-        String url = "https://www.saucedemo.com";
+        String url = ConfigReader.get("url");
         driver.get(url);
         loginPage = new LoginPage(driver);
-        loginPage.setUser("standard_user");
-        loginPage.setPassword("secret_sauce");
+        loginPage.setUser(System.getenv("APP_USER"));
+        loginPage.setPassword(System.getenv("PASSWORD"));
         loginPage.clickLoginButton();
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Login test failed: Expected user to be redirected to the Inventory page after successful login.");
     }

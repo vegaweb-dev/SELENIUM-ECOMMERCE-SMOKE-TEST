63a3855f4722d67d38d484b8d977b7943f0b956b
cvega <carlosjuniorrvega@hotmail.com>
2025-10-27 17:34:32 -0500

    chore(BaseTest): delete unnecessary lines and format. CV

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,7 +3,6 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import utilities.DriverManager;
 
 public class BaseTest {

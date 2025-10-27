277187617f7b54d7b6a12971ba6fe5ea135c712e
cvega <carlosjuniorrvega@hotmail.com>
2025-10-27 17:31:21 -0500

    feat(utilities): implement EnvTest to verify values of config.properties. CV

--- src/test/java/utilities/EnvTest.java
+++ src/test/java/utilities/EnvTest.java
@@ -1,4 +1,10 @@
 package utilities;
 
 public class EnvTest {
+    public static void main(String[] args) {
+        System.out.println("URL: " + System.getenv("URL"));
+        System.out.println("APP_USER: " + System.getenv("APP_USER"));
+        System.out.println("PASSWORD: " + System.getenv("PASSWORD"));
+        System.out.println("BROWSER: " + System.getenv("BROWSER"));
+    }
 }

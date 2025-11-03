6268629b5e106c8cf9e9c5ef4425adaf2262cd93
cvega <carlosjuniorrvega@hotmail.com>
2025-11-03 16:53:47 -0500

    fix(driverManager): enable headless mode configuration for CI tests. CV

--- src/main/java/utilities/DriverManager.java
+++ src/main/java/utilities/DriverManager.java
@@ -3,14 +3,19 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     private static WebDriver driver;
 
     public static void initDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         }
     }

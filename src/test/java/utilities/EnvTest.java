24219c4b79dac3fcf342afdb0910d65dae67cb1e
cvega <carlosjuniorrvega@hotmail.com>
2025-11-03 13:51:18 -0500

    refactor(utilities): adjust password name to APP_PASSWORD for consistency. CV

--- src/test/java/utilities/EnvTest.java
+++ src/test/java/utilities/EnvTest.java
@@ -4,7 +4,7 @@ public class EnvTest {
     public static void main(String[] args) {
         System.out.println("URL: " + System.getenv("URL"));
         System.out.println("APP_USER: " + System.getenv("APP_USER"));
-        System.out.println("PASSWORD: " + System.getenv("PASSWORD"));
+        System.out.println("APP_PASSWORD: " + System.getenv("PASSWORD"));
         System.out.println("BROWSER: " + System.getenv("BROWSER"));
     }
 }
